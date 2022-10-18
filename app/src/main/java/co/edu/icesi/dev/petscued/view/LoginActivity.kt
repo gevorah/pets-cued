1ce9d948bbb35b5b339808b72ca8c1605bdd0c1c
gevorah <jhonsebastianijajiortiz@gmail.com>
2022-10-18 00:53:47 -0500

    login and bottom nav

--- app/src/main/java/co/edu/icesi/dev/petscued/view/LoginActivity.kt
+++ app/src/main/java/co/edu/icesi/dev/petscued/view/LoginActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import co.edu.icesi.dev.petscued.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_login)
     }
 }

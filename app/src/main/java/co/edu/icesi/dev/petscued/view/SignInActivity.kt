f91d7df2c5e1009ec2a085f69aab4bd603935eb2
sebastianvilla17 <1193269834@u.icesi.edu.co>
2022-10-31 12:37:15 -0500

    registro e inicio

--- app/src/main/java/co/edu/icesi/dev/petscued/view/SignInActivity.kt
+++ app/src/main/java/co/edu/icesi/dev/petscued/view/SignInActivity.kt
@@ -3,28 +3,63 @@ package co.edu.icesi.dev.petscued.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import co.edu.icesi.dev.petscued.R
+import co.edu.icesi.dev.petscued.databinding.ActivitySignInBinding
+import co.edu.icesi.dev.petscued.databinding.ActivitySignUpBinding
+import co.edu.icesi.dev.petscued.model.User
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.top_nav.*
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         back_title.setText(R.string.action_sign_in)
         back_btn.setOnClickListener {
             finish()
         }
 
-        sign_in_btn.setOnClickListener {
-            val intent = Intent(this, HomePublicationActivity::class.java)
-            startActivity(intent)
+        val email = binding.emailFld.text.toString()
+        val password = binding.passwordFld.text.toString()
+        binding.signInBtn.setOnClickListener{
+            Firebase.auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+
+                val fbuser = Firebase.auth.currentUser
+                if(fbuser!!.isEmailVerified){
+                   Firebase.firestore.collection("users").document(fbuser.uid).get().addOnSuccessListener {
+                       val user = it.toObject(User::class.java)
+                       saveUser(user!!)
+                       startActivity(Intent(this, HomePublicationActivity::class.java))
+                       finish()
+                   }
+                } else{
+                    Toast.makeText(this, "Su email no ha sido verificado", Toast.LENGTH_LONG).show()
+                }
+
+            }.addOnFailureListener {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+            }
         }
 
         //lost_password_txt.setOnClickListener {
         //    val intent = Intent(this, Activity::class.java)
         //    startActivity(intent)
         //}
+    }
+    fun saveUser ( user: User){
+        val sp = getSharedPreferences("appmoviles", MODE_PRIVATE)
+        val json = Gson().toJson(user)
+        sp.edit().putString("user", json).apply()
     }
 }

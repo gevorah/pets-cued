5a2dc015caf01bfd79b118fc398a7c7f47a25df8
sebastianvilla17 <1193269834@u.icesi.edu.co>
2022-11-29 10:54:36 -0500

    saves comments on firestore

--- app/src/main/java/co/edu/icesi/dev/petscued/view/pets/PetInfoFragment.kt
+++ app/src/main/java/co/edu/icesi/dev/petscued/view/pets/PetInfoFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import co.edu.icesi.dev.petscued.databinding.FragmentPetInfoBinding
+import co.edu.icesi.dev.petscued.model.Comment
 import co.edu.icesi.dev.petscued.model.Publication
+import co.edu.icesi.dev.petscued.view.profile.UserPublicationsFragment
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.util.*
 
 class PetInfoFragment(private val publication : Publication) : Fragment() {
 
@@ -21,6 +27,13 @@ class PetInfoFragment(private val publication : Publication) : Fragment() {
 
         binding.backPetInfoButton.setOnClickListener{
             activity?.supportFragmentManager?.popBackStack()
+        }
+s
+        binding.commentBttn.setOnClickListener{
+            if(publish()){
+                Toast.makeText(this.context, "Su comentario se publicó con exito", Toast.LENGTH_SHORT).show()
+                binding.CommentEditTextMultiLine.setText("")
+            }
         }
 
         return binding.root
@@ -40,4 +53,30 @@ class PetInfoFragment(private val publication : Publication) : Fragment() {
         binding.editTextMultiLineDetails.setText(publication.description)
         binding.textViewContactInfo.text = publication.contactInformation
     }
+
+    private fun publish(): Boolean {
+
+        if(!checkIfNotBlankOrEmpty(binding.CommentEditTextMultiLine.text.toString()))
+            return false
+
+        val valueComment = binding.CommentEditTextMultiLine.text.toString()
+
+        val comment = Comment(
+            UUID.randomUUID().toString(),
+            valueComment,
+            Firebase.auth.currentUser!!.uid,
+            publication.id
+        )
+        Firebase.firestore.collection("comments").document(comment.id).set(comment)
+        return true
+    }
+
+    private fun checkIfNotBlankOrEmpty(field: String): Boolean {
+        if(field.isNotBlank() && field.isNotEmpty()){
+            return true
+        }
+        Toast.makeText(this.context, "Debe escribir un comentario", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
 }
